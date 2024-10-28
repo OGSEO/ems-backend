7ecfe09a4c7898a304059fb7d89df68b2963c59f
Ollie Gelton <olliegelton@gmail.com>
2024-10-28 21:33:15 +0100

    Added method getEmployeeById in all layers

--- src/main/java/nl/gelton/emsbackend/service/EmployeeService.java
+++ src/main/java/nl/gelton/emsbackend/service/EmployeeService.java
@@ -4,4 +4,6 @@ import nl.gelton.emsbackend.dto.EmployeeDto;
 
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
 }
--- src/main/java/nl/gelton/emsbackend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/nl/gelton/emsbackend/service/impl/EmployeeServiceImpl.java
@@ -8,6 +8,8 @@ import nl.gelton.emsbackend.repository.EmployeeRepository;
 import nl.gelton.emsbackend.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
+import java.lang.module.ResolutionException;
+
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
@@ -20,5 +22,14 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResolutionException("Employee does not exist with id: " + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 }
